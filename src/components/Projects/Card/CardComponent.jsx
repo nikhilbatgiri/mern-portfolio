75869357f16371992312fbde00a7712ac5ddb54c
nikhilbatgiri <nikhilbatgiri@gmail.com>
2024-02-08 15:46:06 +0530

    section update

--- src/components/Projects/Card/CardComponent.jsx
+++ src/components/Projects/Card/CardComponent.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import styles from './CardComponent.module.scss';
 
-// const projects = [
-//     {title: 'Portfolio', description: 'A React based web application (SPA) showcasing a professional portfolio with different sections, few UI animations etc.'},
-//     {title: 'Events Listing', description: 'A React based web application (SPA) simply adds, removes an event and it’s details, edits the same, fetches data from backend etc.'},
-//     {title: 'Simple Blog', description: 'A React based web application (SPA) showcasing a simple blog which performs CRUD operations of adding, removing, updating the blog posts and its details, retrieving data from backend etc.'},
-//     {title: 'To-Do App', description: 'A React based web application (SPA) for adding to-do tasks/activities to a list and deleting the tasks from the same.'}
-// ];
-
 const CardComponent = () => {
 
   return (
